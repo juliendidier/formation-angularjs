6302d50a4a535a32fec6d35feac298ca45efe835
Julien DIDIER <julien@didier.io>
2014-11-06 12:18:55 +0100

    display loading and error

--- app/scripts/controllers/users.js
+++ app/scripts/controllers/users.js
@@ -3,7 +3,13 @@
 angular.module('appApp')
     .controller('UsersCtrl', function ($scope, $routeParams, UserRepositories) {
         $scope.username = $routeParams.username;
+        $scope.isLoading = true;
+        $scope.isError = false;
 
-        $scope.repositories = UserRepositories.list({username: $scope.username });
+        $scope.repositories = UserRepositories.list({username: $scope.username }, function () {
+            $scope.isLoading = false;
+        }, function () {
+            $scope.isError = true;
+        });
     })
 ;
