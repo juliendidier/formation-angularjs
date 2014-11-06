2b66b642d02612614affc5bd4b7b24e4393e9cac
Julien DIDIER <julien@didier.io>
2014-11-06 17:29:57 +0100

    display image with filter

---
+++ app/scripts/filters/ghsize.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('appApp')
+    .filter('ghsize', function () {
+        return function(value, size) {
+            return value+(value.indexOf('?') > 0 ? '&' : '?')+'size='+size;
+        }
+    })
