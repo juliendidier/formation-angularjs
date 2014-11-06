f003c46d6b508702be374c1352caa47a6c9839e4
Julien DIDIER <julien@didier.io>
2014-11-06 14:36:15 +0100

    readds otherwise

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,30 +9,17 @@
  * Main module of the application.
  */
 angular
-  .module('appApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngSanitize',
-    'ui.router',
-    'ngTouch'
-  ])
-    // .config(function ($stateProvider) {
-    //     $stateProvider
-    //     .when('/', {
-    //         templateUrl: 'views/main.html',
-    //         controller: 'MainCtrl'
-    //     })
-    //     .when('/about', {
-    //         templateUrl: 'views/about.html',
-    //         controller: 'AboutCtrl'
-    //     })
-    //     .when('/users/:username', {
-    //         templateUrl: 'views/users_show.html',
-    //         controller: 'UsersCtrl'
-    //     })
-    //     .otherwise({
-    //         redirectTo: '/'
-    //     });
-    // })
+    .module('appApp', [
+        'ngAnimate',
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ui.router',
+        'ngTouch'
+    ])
+    .config(function ($urlRouterProvider) {
+        $urlRouterProvider
+            .otherwise('/')
+        ;
+    })
 ;
